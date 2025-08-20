a09baa479ce181040187d869f32b680ce096bfa2
owomed <owomedsunucu@gmail.com>
2025-08-20 22:49:18 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,20 +1,45 @@
-const Discord = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
+    // Prefix Komut Adı
     name: '3ping',
-    description: 'Botun gecikmesini hesaplar',
-    execute(client, message, args) {
-        message.reply('`Ping hesaplanıyor...`').then(sentMessage => {
-            const ping = sentMessage.createdTimestamp - message.createdTimestamp;
-            const apiPing = Math.round(client.ws.ping);
+    // Prefix Komut Takma Adları (İsteğe bağlı)
+    aliases: ['ping', 'gecikme'],
 
-            const embed = new Discord.MessageEmbed()
-                .setTitle('Pong!')
-                .setDescription(`Botun gecikmesi: **${ping}ms**\nAPI gecikmesi: **${apiPing}ms**`)
-                .setColor('#00ff00')
-                .setTimestamp();
+    // Slash komutu için gerekli builder yapısı
+    data: new SlashCommandBuilder()
+        .setName('3ping')
+        .setDescription('Botun gecikmesini hesaplar.'),
 
-            sentMessage.edit('', embed);
-        });
-    }
+    // Prefix ve Slash komutları için ortak işlev gören ana fonksiyon
+    async execute(interaction) {
+        let sentMessage;
+
+        // Komutun prefix mi yoksa slash mı olduğunu kontrol et
+        if (interaction.isChatInputCommand) {
+            // Slash komutu için geçici bir cevap gönder
+            await interaction.reply({ content: '`Ping hesaplanıyor...`', fetchReply: true })
+            .then(message => sentMessage = message);
+        } else {
+            // Prefix komutu için normal bir cevap gönder
+            sentMessage = await interaction.reply('`Ping hesaplanıyor...`');
+        }
+
+        const ping = sentMessage.createdTimestamp - interaction.createdTimestamp;
+        const apiPing = Math.round(interaction.client.ws.ping);
+
+        const embed = new EmbedBuilder()
+            .setTitle('Pong!🏓')
+            .setDescription(`Botun gecikmesi: **${ping}ms**\nAPI gecikmesi: **${apiPing}ms**`)
+            .setColor('#00ff00')
+            .setTimestamp();
+
+        if (interaction.isChatInputCommand) {
+            // Slash komutu için cevabı düzenle
+            await interaction.editReply({ content: null, embeds: [embed] });
+        } else {
+            // Prefix komutu için cevabı düzenle
+            await sentMessage.edit({ content: null, embeds: [embed] });
+        }
+    },
 };
